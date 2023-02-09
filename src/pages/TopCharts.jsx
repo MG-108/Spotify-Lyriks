8de67abdaec9bf052ff96c61079cf8cbced6e11d
Matheus Ghignatti <matheusghignatti@gmail.com>
2023-02-09 16:05:20 -0300

    Top Charts page

--- src/pages/TopCharts.jsx
+++ src/pages/TopCharts.jsx
@@ -1,3 +1,37 @@
-const TopCharts = () => <div>TopCharts</div>;
+import { useSelector } from "react-redux";
+
+import { Error, Loader, SongCard } from "../components";
+import { useGetRapTopChartsQuery } from "../redux/services/shazamCore";
+
+const TopCharts = () => {
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { data, isFetching, error } = useGetRapTopChartsQuery();
+
+  if (isFetching) return <Loader title="Loading top charts" />;
+
+  if (error) return <Error />;
+
+  // console.log(data);
+  return (
+    <div className="flex flex-col">
+      <h2 className="mt-4 mb-10 text-left text-3xl font-bold text-white">
+        Discover Top Charts
+      </h2>
+
+      <div className="flex flex-wrap justify-center gap-8 sm:justify-start">
+        {data?.map((song, i) => (
+          <SongCard
+            key={song.key}
+            song={song}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            data={data}
+            i={i}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default TopCharts;
